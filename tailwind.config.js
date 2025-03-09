7001dc00573be83357a9510f2411d071602ffa89
Shayan Kolahi <83598541+shayan-kolahi@users.noreply.github.com>
2025-03-09 22:43:18 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+import PrimeUI from 'tailwindcss-primeui';
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [PrimeUI]
+}
+
