ab455a06b19bcc6e90a22097864a94fcf3c4b055
Shayan Kolahi <83598541+shayan-kolahi@users.noreply.github.com>
2025-03-10 21:44:50 +0330

    first config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import {HomeComponent} from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
+  imports: [
+    HomeComponent
+  ],
   templateUrl: './app.component.html'
 })
 export class AppComponent {}
