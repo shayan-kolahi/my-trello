c251ea97b8dda6ae79cac0346665f79211db82b1
Shayan Kolahi <83598541+shayan-kolahi@users.noreply.github.com>
2025-03-25 22:00:02 +0330

    first config

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {Button} from 'primeng/button';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    Button
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
